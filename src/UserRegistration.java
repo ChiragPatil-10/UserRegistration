3b3176a8cbee232748a4fbfa923ab2cf6d4b4fcd
Chirag Patil <patilchirag192k@gmail.com>
2024-12-10 16:42:56 +0530

    Added Last Name

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -1,26 +1,33 @@
 import java.util.Scanner;
+import java.util.regex.*;
 
 public class UserRegistration {
-
-    private static boolean isValidFirstName(String name) {
-
-        return name.matches("[A-Z][a-z]{2,}");
+    public static boolean isValidFirstName(String firstName) {
+        String regex = "^[A-Z][a-zA-Z]{2,}$";
+        return Pattern.matches(regex, firstName);
     }
-
+    public static boolean isValidLastName(String lastName){
+        String regex = "^[A-Z][a-zA-Z]{2,}$";
+        return Pattern.matches(regex, lastName);
+    }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the User Registration Program");
-
+        System.out.println("Welcome to User Registration Problem");
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter First name: ");
+        System.out.println("Enter the First Name: ");
         String firstName = sc.nextLine();
-
-        if(isValidFirstName(firstName)) {
-            System.out.println("First name is Valid!!");
+        if (isValidFirstName(firstName)) {
+            System.out.println("Valid first name.");
         } else {
-            System.out.println("Please enter a valid First name starting with Cap and has minimum 3 characters");
+            System.out.println("Invalid first name. It must start with a capital letter and have at least 3 characters.");
         }
-    }
+        System.out.println("Enter the Last Name");
+        String lastName = sc.nextLine();
+        if (isValidLastName(lastName)){
+            System.out.println("Valid Last name.");
+        }else {
+            System.out.println("Invalid last name. It must start with a capital letter and have at least 3 characters.");
+        }
 
+    }
 }
