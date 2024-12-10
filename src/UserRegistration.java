44b5699a96746bf97b951a95bb13a20a1d27f78c
Chirag Patil <patilchirag192k@gmail.com>
2024-12-10 11:33:47 +0530

    added: Enter the valid first name

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -2,8 +2,25 @@ import java.util.Scanner;
 
 public class UserRegistration {
 
+    private static boolean isValidFirstName(String name) {
+
+        return name.matches("[A-Z][a-z]{2,}");
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to the User Registration Program");
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter First name: ");
+        String firstName = sc.nextLine();
+
+        if(isValidFirstName(firstName)) {
+            System.out.println("First name is Valid!!");
+        } else {
+            System.out.println("Please enter a valid First name starting with Cap and has minimum 3 characters");
+        }
     }
+
 }
