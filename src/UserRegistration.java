e339f51a4aa26c74dd3ec83f618c951a004302dc
Chirag Patil <patilchirag192k@gmail.com>
2024-12-10 11:30:39 +0530

    Added : welcome to user registration

---
+++ src/UserRegistration.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class UserRegistration {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the User Registration Program");
+
+    }
+}
